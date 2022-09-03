22b9beecf1d2e34537e3868adbc4142405e39361
noman <nomannabil30@gmail.com>
2022-09-03 08:13:21 +0600

    daynamic news details data added at modal

--- js/news.js
+++ js/news.js
@@ -17,61 +17,63 @@ const displaymenuItems = (menuItemsData) => {
     const { category_id, category_name } = menuItem;
 
     const menuItemsList = document.createElement('li');
-    menuItemsList.innerHTML = `<button class="btn btn-ghost text-zinc-500" onclick='fetchNewsData("${category_id}")'>${category_name}</button>`;
+    menuItemsList.innerHTML = `<button class="btn btn-ghost text-zinc-500" onclick='fetchNewsData("${category_id}","${category_name}")'>${category_name}</button>`;
 
     targetMenuBar.appendChild(menuItemsList);
   });
 };
 
-const fetchNewsData = async (category_id) => {
+const fetchNewsData = async (category_id,category_name) => {
   try {
     let url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
     let res = await fetch(url);
     let data = await res.json();
-    displayCatagoryWiseNews(data.data);
+    displayCatagoryWiseNews(data.data,category_name);
   } catch (error) {
     console.log(error.stack);
   }
 };
 
-const displayCatagoryWiseNews = (newsData) => {
-  console.log(newsData);
+const displayCatagoryWiseNews = (newsData,category_name) => {
+    // console.log(newsData);
+    // console.log(category_name);
 
-    const targetNotFoundSec = document.querySelector('#not-found-section');
-    const targetNewsContainer = document.querySelector('#news-container');
-    targetNewsContainer.innerHTML = '';
-    if (newsData.length === 0) {
-    
-        targetNotFoundSec.classList.remove('hidden');
-        return;
-    }
-    targetNotFoundSec.classList.add('hidden');
+  const targetNotFoundSec = document.querySelector('#not-found-section');
+  const targetNewsContainer = document.querySelector('#news-container');
+  targetNewsContainer.innerHTML = '';
+  if (newsData.length === 0) {
+    targetNotFoundSec.classList.remove('hidden');
+    return;
+  }
+  targetNotFoundSec.classList.add('hidden');
 
-    newsData.forEach((news) => {
-      const {
-        title,
-        total_view,
-        author: { name, published_date },
-        thumbnail_url,
-        image_url,
-        details,
-      } = news;
+  newsData.forEach((news) => {
+    const {
+      _id,
+      title,
+      total_view,
+      author: { name, published_date },
+      thumbnail_url,
+      details,
+    } = news;
 
-      const newsDiv = document.createElement('div');
+    const newsDiv = document.createElement('div');
       newsDiv.innerHTML = `<!-- card-news-1 -->
-    <div class="card lg:card-side bg-base-100 shadow-xl my-6 p-4">
+      
+    <div class="card lg:card-side bg-base-100 shadow-xl my-6 p-4" onclick='fetchNewsDetailsData("${_id}")'>
+    
       <figure>
         <img
           src="${thumbnail_url}"
           alt="Movie"
-          class="rounded-lg h-48 w-22 object-cover md:h-80 lg:h-full"
+          class="rounded-lg h-48 min-w-full object-fill md:h-80 lg:h-full"
         />
       </figure>
       <div class="card-body">
-        <h2 class="card-title">New movie is released!</h2>
-        <p>Click the button to watch on Jetflix app.</p>
-        <p>Click the button to watch on Jetflix app.</p>
-        <p>Click the button to watch on Jetflix app.</p>
+        <h2 class="card-title">${title}</h2>
+        <p class="text-neutral-400">${
+          details.length > 200 ? details.slice(0, 200) + '...' : details
+        }</p>
         <div class=" flex flex-wrap justify-center sm:justify-between items-center text-center">
           <div class="flex flex-col sm:flex-row items-center justify-center">
             <img
@@ -80,13 +82,15 @@ const displayCatagoryWiseNews = (newsData) => {
               class="w-16 h-16 rounded-full mx-auto"
             />
             <div class="mx-4 text-center ">
-              <h2 class="font-semibold">Name</h2>
-              <h3 class="text-slate-500">Date</h3>
+              <h2 class="font-semibold">${name ? name : 'No-Author'}</h2>
+              <h3 class="text-slate-500">${
+                published_date ? published_date.slice(0, 10) : 'Not-Available'
+              }</h3>
             </div>
           </div>
           <h1>
             <i class="fa-regular fa-eye"></i>
-            <span class="font-semibold">1.5M</span>
+            <span class="font-semibold">${total_view ? total_view : '00'}</span>
           </h1>
           <div class="rating rating-sm">
             <input
@@ -117,14 +121,57 @@ const displayCatagoryWiseNews = (newsData) => {
             />
           </div>
           <button class="text-indigo-500 ml-5 md:ml-0">
-            <i class="fa-solid fa-arrow-right"></i>
+          <label for="my-modal-3" class="btn text-indigo-500 bg-white border-0 hover:bg-white"> <i class="fa-solid fa-arrow-right"></i></label>
+           
           </button>
         </div>
       </div>
     </div>`;
 
-      targetNewsContainer.appendChild(newsDiv);
-    });
+    targetNewsContainer.appendChild(newsDiv);
+  });
+};
+
+const fetchNewsDetailsData = async (news_id) => {
+  try {
+    let url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+    let res = await fetch(url);
+    let data = await res.json();
+    displayNewsDetailsOnModal(data.data[0]);
+  } catch (error) {
+    console.log(error.stack);
+  }
+};
+
+const displayNewsDetailsOnModal = (newsDetailsData) => {
+    console.log(newsDetailsData);
+    const { title, thumbnail_url, details, author: { name, published_date }, total_view } = newsDetailsData;
+    
+    const targetModalBody = document.querySelector('#modal-body');
+    targetModalBody.innerHTML = `<div class="card card-compact w-full p-4 bg-base-100 shadow-xl">
+    <figure>
+      <img src="${thumbnail_url}" alt="Shoes" class="rounded-lg h-48 min-w-full object-fill md:h-80 lg:h-full" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">${title}</h2>
+      <p class="text-neutral-400">${
+        details.length > 200 ? details.slice(0, 200) + '...' : details
+      }</p>
+      
+      <div class="flex  items-center justify-between">
+        <div>
+        <p class="text-xs">${name ? name : 'No-Author'}</p>
+        <p class="text-xs">${
+            published_date ? published_date.slice(0, 10) : 'Not-Available'
+          }</p>
+        </div>
+        <div><p class="text-xs" >View: ${total_view ? total_view : '00'}</p></div>
+      </div>
+    </div>
+  </div>`;
+
+
+    
 };
 
 fetchMenuItemsData('');
